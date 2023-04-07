78c48587896b37c76ccfb3a32ebdb882402cf4ad
Bogdan <bogdanvukomanovic@gmail.com>
2023-04-07 21:01:02 +0200

    Started implementing Command line interface.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,12 +5,58 @@ import job_queue.ScanningJob;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 
 public class Main {
 
+//    public enum Command {
+//
+//        ADD_DIRECTORY("ad"),
+//        ADD_WEB("aw");
+//
+//        private String command;
+//
+//        Command(String command) {
+//            this.command = command;
+//        }
+//
+//        public String getCommand() {
+//            return command;
+//        }
+//
+//    }
+
+    private class Command {
+
+        final static String ADD_DIRECTORY = "ad";
+
+    }
+
+    private void CLI() {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            String line = sc.nextLine();
+
+            switch (line) {
+
+                case Command.ADD_DIRECTORY:
+                    System.out.println("Command: Add directory");
+                    break;
+
+                default:
+                    throw new IllegalStateException("Unexpected value: " + line);
+
+            }
+
+        }
+
+    }
     public static void main(String[] args) {
 
         Configuration.load();
