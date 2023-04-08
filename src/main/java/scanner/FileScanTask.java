2758e40f6bf179134651d73cbfa331158535aafc
Bogdan <bogdanvukomanovic@gmail.com>
2023-04-08 16:54:17 +0200

    Added blueprint for scanner components.

---
+++ src/main/java/scanner/FileScanTask.java
@@ -0,0 +1,13 @@
+package scanner;
+
+import java.util.Map;
+import java.util.concurrent.RecursiveTask;
+
+public class FileScanTask extends RecursiveTask<Map<String, Integer>> {
+
+    @Override
+    protected Map<String, Integer> compute() {
+        return null;
+    }
+
+}
---
+++ src/main/java/scanner/FileScanner.java
@@ -0,0 +1,11 @@
+package scanner;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class FileScanner {
+
+    ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+
+
+}
