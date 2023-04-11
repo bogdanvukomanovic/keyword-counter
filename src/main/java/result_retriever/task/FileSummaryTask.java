e2944a77075cd8b1f8047f761d88a8d2f1fc09a0
Bogdan <bogdanvukomanovic@gmail.com>
2023-04-11 23:14:19 +0200

    Implemented FileSummaryTask. Needs testing.

--- src/main/java/result_retriever/task/FileSummaryTask.java
+++ src/main/java/result_retriever/task/FileSummaryTask.java
@@ -1,51 +1,45 @@
 package result_retriever.task;
 
-import directory_crawler.Corpus;
-import job_queue.job.FileScanningJob;
-import job_queue.job.ScanningJob;
+import result_retriever.result.Result;
+import result_retriever.result.ResultType;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public class FileSummaryTask implements Callable<Map<String, Map<String, Integer>>> {
+public class FileSummaryTask implements Callable<Map<String, Result>> {
 
-    private Map<String, Future<Map<String, Integer>>> results;
+    private Map<String, Result> results;
 
-    public FileSummaryTask(Map<String, Future<Map<String, Integer>>> results) {
+    public FileSummaryTask(Map<String, Result> results) {
         this.results = results;
     }
 
     @Override
-    public Map<String, Map<String, Integer>> call() throws Exception {
-
-//        results.entrySet().stream().filter(x -> );
-
-        return null;
-    }
-
-
-    public static void main(String[] args) {
-
-        Map<String, Integer> result_1 = new HashMap<>();
-        Map<String, Integer> result_2 = new HashMap<>();
-
-        result_1.put("A", 1);
-        result_1.put("A", 2);
-        result_2.put("B", 69);
-
-        Map<String, Map<String, Integer>> results = new HashMap<>();
-
-        results.put("AA", result_1);
-        results.put("BB", result_2);
-
-
-        results.entrySet().stream().filter(x -> x.getKey() == "AA").
-                forEach(System.out::println);
+    public Map<String, Result> call() throws Exception {
+        return getFileTypeEntries(results);
 
     }
 
+    private boolean isFile(Result result) {
+
+        if (result.getType().equals(ResultType.FILE)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private <K, V> Map<K, V> getFileTypeEntries(Map<K, V> map) {
+
+        Predicate<V> predicate = value -> isFile((Result) value);
+
+        return map.entrySet().stream()
+                .filter(entry -> predicate.test(entry.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 
 }
 
